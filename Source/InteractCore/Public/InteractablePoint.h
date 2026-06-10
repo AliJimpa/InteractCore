cd1edd273c9e3bfaa2f40c3d3cae8a7d5c6db172
Alijimpa <alibadpa007@gmail.com>
2026-06-10 13:20:07 +0330

    Create InteractablePointy Componetn

---
+++ Source/InteractCore/Public/InteractablePoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractableZone.h"
+#include "InteractablePoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTCORE_API UInteractablePoint : public UInteractableZone
+{
+	GENERATED_BODY()
+	
+};
