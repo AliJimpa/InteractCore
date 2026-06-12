94ae1bd1124fd95598cbfd64e230be95d9fa444b
Alijimpa <alibadpa007@gmail.com>
2026-06-12 14:12:20 +0330

    Implement Input and Condition for Interaction

--- Source/InteractCore/Private/InteractableComponent.cpp
+++ Source/InteractCore/Private/InteractableComponent.cpp
@@ -19,15 +19,18 @@ void UInteractableComponent::OnRegister()
     SetCollisionResponseToChannel(InteractChannel, ECR_Block);
 }
 
-void UInteractableComponent::BeginPlay()
-{
-    Super::BeginPlay();
-    if (!StartWithAllowed)
-        bIsEnableInteraction = false;
-}
-
 void UInteractableComponent::Interact_Implementation(UInteractionComponent *Provider)
 {
+    if (InteractMode != EInteractionUsageMode::Multi)
+    {
+        bHasInteracted = true;
+    }
+    if (InteractMode == EInteractionUsageMode::Cooldown)
+    {
+        const UWorld *World = GetWorld();
+        if (World != nullptr)
+            NextAllowedInteractTime = World->GetTimeSeconds() + CooldowDuration;
+    }
     OnInteract.Broadcast(Provider);
     // LOG("INTERACT");
 }
@@ -45,11 +48,90 @@ void UInteractableComponent::UnHover_Implementation(UInteractionComponent *Provi
 }
 bool UInteractableComponent::ShouldHandleInput_Implementation(const FInputActionInstance &InputValue) const
 {
-    LOG("InputValue")
-    return true;
+    if (!CanInteract())
+        return false;
+
+    const ETriggerEvent TriggerEvent = InputValue.GetTriggerEvent();
+
+    switch (InputMode)
+    {
+    case EInteractionInputMode::Press:
+    {
+        return TriggerEvent == ETriggerEvent::Started;
+    }
+    case EInteractionInputMode::Release:
+    {
+        return TriggerEvent == ETriggerEvent::Completed;
+    }
+    case EInteractionInputMode::Tap:
+    {
+        // Best if Input Action has a Tap trigger configured
+        return TriggerEvent == ETriggerEvent::Triggered;
+    }
+    case EInteractionInputMode::Hold:
+    {
+        // If using Hold Trigger in Input Action, Triggered is enough.
+        // Otherwise use elapsed time.
+        return (TriggerEvent == ETriggerEvent::Ongoing || TriggerEvent == ETriggerEvent::Triggered) && InputValue.GetElapsedTime() >= HoldTimeThreshold;
+    }
+    case EInteractionInputMode::ChargedRelease:
+    {
+        // If using Hold Trigger in Input Action, Triggered is enough.
+        // Otherwise use elapsed time.
+        return TriggerEvent == ETriggerEvent::Triggered && InputValue.GetElapsedTime() >= HoldTimeThreshold;
+    }
+    case EInteractionInputMode::DoubleClick:
+    {
+        const UWorld *World = GetWorld();
+        if (!World || TriggerEvent != ETriggerEvent::Started)
+        {
+            return false;
+        }
+
+        const float CurrentTime = World->GetTimeSeconds();
+        const bool bIsDoubleClick = (CurrentTime - LastPressTime) <= DoubleTapInterval;
+        LastPressTime = CurrentTime;
+        return bIsDoubleClick;
+    }
+    case EInteractionInputMode::Any:
+    {
+        return TriggerEvent == ETriggerEvent::Started || TriggerEvent == ETriggerEvent::Triggered || TriggerEvent == ETriggerEvent::Completed || TriggerEvent == ETriggerEvent::Ongoing || TriggerEvent == ETriggerEvent::Canceled;
+    }
+    default:
+    {
+        return false;
+    }
+    }
 }
 
 bool UInteractableComponent::CanInteract() const
 {
-    return true;
+    const UWorld *World = GetWorld();
+    if (!World)
+    {
+        return false;
+    }
+
+    switch (InteractMode)
+    {
+    case EInteractionUsageMode::Once:
+    {
+        return !bHasInteracted;
+    }
+
+    case EInteractionUsageMode::Multi:
+    {
+        return true;
+    }
+
+    case EInteractionUsageMode::Cooldown:
+    {
+        return World->GetTimeSeconds() >= NextAllowedInteractTime;
+    }
+
+    default:
+    {
+        return false;
+    }
+    }
 }
--- Source/InteractCore/Public/InteractableComponent.h
+++ Source/InteractCore/Public/InteractableComponent.h
@@ -9,14 +9,22 @@
 #include "InteractableComponent.generated.h"
 
 UENUM()
-enum class EInputInteractionType : uint8
+enum class EInteractionInputMode : uint8
 {
 	Press UMETA(DisplayName = "Press"),
 	Release UMETA(DisplayName = "Release"),
 	Tap UMETA(DisplayName = "Tap"),
 	Hold UMETA(DisplayName = "Hold"),
+	ChargedRelease UMETA(DisplayName = "ChargedRelease"),
 	DoubleClick UMETA(DisplayName = "Double Click"),
 	Any UMETA(DisplayName = "Any")
+};
+UENUM()
+enum class EInteractionUsageMode : uint8
+{
+	Once UMETA(DisplayName = "Once"),
+	Multi UMETA(DisplayName = "Multi"),
+	Cooldown UMETA(DisplayName = "Cooldown")
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractionEvent, UInteractionComponent *, Provider);
@@ -30,7 +38,6 @@ public:
 
 protected:
 	virtual void OnRegister() override;
-	virtual void BeginPlay() override;
 
 protected:
 	// Implement IInteraction Interface
@@ -44,7 +51,9 @@ private:
 
 private:
 	bool bIsHovered = false;
-	bool bIsEnableInteraction = true;
+	bool bHasInteracted = false;
+	float NextAllowedInteractTime = 0.0f;
+	mutable float LastPressTime = -1000.f;
 
 public:
 	UPROPERTY(BlueprintAssignable, Category = "Hover Events")
@@ -54,21 +63,41 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Interact Events")
 	FOnInteractionEvent OnInteract;
 
+private:
+	UPROPERTY(EditAnywhere, Category = "Interaction", meta = (AllowPrivateAccess = "true"))
+	bool EnableInteraction = true;
+
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction")
-	bool StartWithAllowed = true;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
 	TEnumAsByte<ECollisionChannel> InteractChannel = ECC_Camera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+	EInteractionUsageMode InteractMode = EInteractionUsageMode::Once;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction", meta = (EditCondition = "InteractMode == EInteractionUsageMode::Cooldown", EditConditionHides, ClampMin = "0.0"))
+	float CooldowDuration = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction|Input")
+	EInteractionInputMode InputMode = EInteractionInputMode::Press;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction|Input", meta = (EditCondition = "InputMode == EInteractionInputMode::Hold ", EditConditionHides))
+	float HoldTimeThreshold = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction|Input", meta = (EditCondition = "InputMode == EInteractionInputMode::DoubleClick ", EditConditionHides))
+	float DoubleTapInterval = 0.3f;
 
 public:
 	UFUNCTION(BlueprintPure, Category = "Interaction|Status")
 	bool IsHovered() const { return bIsHovered; }
 	UFUNCTION(BlueprintPure, Category = "Interaction|Status")
-	bool IsEnableInteraction() const { return bIsEnableInteraction; }
-	UFUNCTION(BlueprintCallable, Category = "Interaction|Status")
+	bool IsEnableInteraction() const { return EnableInteraction; }
+	UFUNCTION(BlueprintPure, Category = "Interaction|Status")
+	bool HasInteracted() const { return bHasInteracted; }
+	UFUNCTION(BlueprintCallable, Category = "Interaction|Functions")
 	void SetEnableInteraction(bool Enable)
 	{
-		bIsEnableInteraction = Enable;
+		EnableInteraction = Enable;
+	}
+	UFUNCTION(BlueprintCallable, Category = "Interaction|Functions")
+	void ResetInteraction()
+	{
+		bHasInteracted = false;
+		NextAllowedInteractTime = 0.0f;
 	}
 
 protected:
@@ -76,6 +105,6 @@ protected:
 	bool IsAllowedInteraction(UInteractionComponent *Interactor) const;
 	virtual bool IsAllowedInteraction_Implementation(UInteractionComponent *Interactor) const
 	{
-		return bIsEnableInteraction;
+		return EnableInteraction && !bHasInteracted;
 	}
 };
