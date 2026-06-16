fbc067ff81202e5da212b74fb67944725b5383e9
Alijimpa <alibadpa007@gmail.com>
2026-06-16 22:06:59 +0330

    Add Recomended Header

--- Source/InteractCore/Private/Interactable/InteractableDetection.cpp
+++ Source/InteractCore/Private/Interactable/InteractableDetection.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "InteractableDetection.h"
+#include "InteractionComponent.h"
 #if WITH_EDITOR
 #include "DrawDebugHelpers.h"
 #endif
--- Source/InteractCore/Private/Interactable/InteractablePoint.cpp
+++ Source/InteractCore/Private/Interactable/InteractablePoint.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "InteractablePoint.h"
+#include "Interactable.h"
 
 UInteractablePoint::UInteractablePoint()
 {
