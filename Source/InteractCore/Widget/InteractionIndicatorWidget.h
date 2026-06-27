5d3a6a694aa341f72d176e7122ea80dcf994ab80
Alijimpa <alibadpa007@gmail.com>
2026-06-27 21:03:50 +0330

    Refine Widget based on new interface

--- Source/InteractCore/Widget/InteractionIndicatorWidget.h
+++ Source/InteractCore/Widget/InteractionIndicatorWidget.h
@@ -4,43 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Interface/InteractionIndicator.h"
 #include "InteractionIndicatorWidget.generated.h"
 
 class UInteractableComponent;
 
-UENUM(BlueprintType)
-enum class EInteractionState : uint8
-{
-	Begindetection UMETA(DisplayName = "Begin Detection"),
-	Enddetection UMETA(DisplayName = "End Detection"),
-	Beginhover UMETA(DisplayName = "Begin Hover"),
-	Endhover UMETA(DisplayName = "End Hover")
-};
 /**
  *
  */
 UCLASS(Abstract, Blueprintable, BlueprintType)
-class INTERACTCORE_API UInteractionIndicatorWidget : public UUserWidget
+class INTERACTCORE_API UInteractionIndicatorWidget : public UUserWidget, public IInteractionIndicator
 {
 	GENERATED_BODY()
+protected:
+	// Implement IInteractionIndicator Interface
+	virtual void InitializeIndicator_Implementation(UInteractableComponent *InOwner) override { OwnerInteractable = InOwner; }
+	virtual void OnInteractionStateChanged_Implementation(EInteractionState NewState) override {}
+	virtual void OnInteractionProgress_Implementation(float Progress) override {}
+	virtual void OnInteractionCompleted_Implementation() override {}
+
 private:
 	UPROPERTY()
 	UInteractableComponent *OwnerInteractable = nullptr;
 
 public:
-	UFUNCTION(BlueprintNativeEvent, Category = "Interaction|Override")
-	void InitializeIndicator(UInteractableComponent *InOwner);
-	virtual void InitializeIndicator_Implementation(UInteractableComponent *InOwner) { OwnerInteractable = InOwner;}
 	UFUNCTION(BlueprintPure, Category = "Interaction|Getter", meta = (DisplayName = "Get Owner Interactable"))
 	UInteractableComponent *GetOwnerInteractable() const { return OwnerInteractable; }
-
-	UFUNCTION(BlueprintNativeEvent, Category = "Interaction|Override")
-	void OnInteractionStateChanged(EInteractionState NewState);
-	virtual void OnInteractionStateChanged_Implementation(EInteractionState NewState) {}
-	UFUNCTION(BlueprintNativeEvent, Category = "Interaction|Override")
-	void OnInteractionProgress(float Progress);
-	virtual void OnInteractionProgress_Implementation(float Progress) {}
-	UFUNCTION(BlueprintNativeEvent, Category = "Interaction|Override")
-	void OnInteractionCompleted();
-	virtual void OnInteractionCompleted_Implementation() {}
 };
