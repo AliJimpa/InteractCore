a884a7bf926735f0fa850beed224ce6e8a873b92
Alijimpa <alibadpa007@gmail.com>
2026-06-27 20:25:09 +0330

    Add Tooltip for Interface MemberFucntions

--- Source/InteractCore/Interface/Interactable.h
+++ Source/InteractCore/Interface/Interactable.h
@@ -17,12 +17,12 @@ class INTERACTCORE_API IInteractable
     GENERATED_BODY()
 
 public:
-    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events", meta = (ToolTip = "Need Tip"))
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events", meta = (ToolTip = "Called when an interaction provider starts hovering this object."))
     void Hover(UInteractionComponent *Provider, const FHitResult &Hit);
-    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events")
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events", meta = (ToolTip = "Called when an interaction provider stops hovering this object."))
     void UnHover(UInteractionComponent *Provider);
-    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events")
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Events", meta = (ToolTip = "Called when an interaction is performed on this object."))
     void Interact(UInteractionComponent *Provider, const FHitResult &Hit, const FInputActionInstance &Instance);
-    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Input")
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interactable|Input", meta = (ToolTip = "Determines whether this interactable should handle the given input action."))
     bool ShouldHandleInput(const FInputActionInstance &InputValue) const;
 };
